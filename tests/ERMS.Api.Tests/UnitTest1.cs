3e470e63c84587c46a3e776a0bc67c6ca5fa35e2
p_basiri <p_basiri@tamin.org>
2026-07-22 11:30:25 +0330

    chore(solution): initialize enterprise solution

---
+++ tests/ERMS.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ERMS.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/ERMS.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ERMS.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
