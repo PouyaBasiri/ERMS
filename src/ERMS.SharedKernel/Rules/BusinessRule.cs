8e52fea3bf33e79129159356e6166a79403260f3
p_basiri <p_basiri@tamin.org>
2026-07-27 17:40:30 +0330

    feat(shared-kernel): add business rules infrastructure

---
+++ src/ERMS.SharedKernel/Rules/BusinessRule.cs
@@ -0,0 +1,13 @@
+﻿using ERMS.SharedKernel.Errors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERMS.SharedKernel.Rules
+{
+    public abstract class BusinessRule : IBusinessRule
+    {
+        public abstract bool IsBroken();
+        public abstract Error Error { get; }
+    }
+}
---
+++ src/ERMS.SharedKernel/Rules/IBusinessRule.cs
@@ -0,0 +1,13 @@
+﻿using ERMS.SharedKernel.Errors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERMS.SharedKernel.Rules
+{
+    public interface IBusinessRule
+    {
+        bool IsBroken();
+        Error Error { get; }
+    }
+}
