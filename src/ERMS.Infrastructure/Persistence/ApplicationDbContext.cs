2e0c09e84d54d49c22f5bdacda8646fe66cd9e25
p_basiri <p_basiri@tamin.org>
2026-07-28 16:19:02 +0330

    feat(infrastructure): add ef core infrastructure

---
+++ src/ERMS.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ERMS.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERMS.Infrastructure.Persistence
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
