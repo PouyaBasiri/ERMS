1582cbc8645bf32510454b7e60ea2244fd393952
p_basiri <p_basiri@tamin.org>
2026-07-29 09:17:04 +0330

    feat(infrastructure): add interceptors user repository

---
+++ src/ERMS.Infrastructure/Services/DomainEventDispatcher.cs
@@ -0,0 +1,30 @@
+﻿using ERMS.SharedKernel;
+using ERMS.SharedKernel.Events;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERMS.Infrastructure.Services
+{
+    public sealed class DomainEventDispatcher : IDomainEventDispatcher
+    {
+        private readonly IPublisher _publisher;
+
+        public DomainEventDispatcher(IPublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public async Task DispatchAsync(
+            IReadOnlyCollection<IDomainEvent> events,
+            CancellationToken cancellationToken = default)
+        {
+            foreach (var domainEvent in events)
+            {
+                await _publisher.Publish(domainEvent, cancellationToken);
+            }
+        }
+    }
+}
