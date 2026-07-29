23a0660c2f4ccced2edb4439982ba31e782dd216
p_basiri <p_basiri@tamin.org>
2026-07-29 13:35:00 +0330

    feat(api): add result to http mapping

--- src/ERMS.Api/Users/CreateUserEndpoint.cs
+++ src/ERMS.Api/Users/CreateUserEndpoint.cs
@@ -1,18 +1,18 @@
 ﻿using ERMS.Api.Endpoints;
+using ERMS.Api.Extensions;
 using ERMS.Domain.Users.CreateUser;
 using MediatR;
 
 namespace ERMS.Api.Users
 {
-    public static class CreateUserEndpoint 
+    public sealed class CreateUserEndpoint : IEndpoint 
     { 
-        public static void Map(RouteGroupBuilder group) 
-        { 
-            group.MapPost("/", HandleAsync).WithName("CreateUser");
-        } 
-        private static async Task<IResult> HandleAsync(CreateUserCommand command, ISender sender, CancellationToken cancellationToken) 
-        { 
-            var result = await sender.Send(command, cancellationToken); return Results.Ok(result);
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/users", HandleAsync).WithName("CreateUser").WithTags("Users"); 
         }
+        private static async Task<IResult> HandleAsync(CreateUserCommand command, ISender sender, CancellationToken cancellationToken)
+        { var result = await sender.Send(command, cancellationToken); return result.ToHttpResult(); 
+        } 
     }
 }
