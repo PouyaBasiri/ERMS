bc6b92e6781ebf4976eb6dd2cdfa32629f830355
p_basiri <p_basiri@tamin.org>
2026-07-29 12:16:53 +0330

    feat(api): create api host

--- src/ERMS.Api/Program.cs
+++ src/ERMS.Api/Program.cs
@@ -1,41 +1,18 @@
+using ERMS.Application;
+using ERMS.Infrastructure;
+using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(); 
 
-var app = builder.Build();
+builder.Services.AddApplication(); 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+builder.Services.AddInfrastructure(builder.Configuration); 
 
-app.UseHttpsRedirection();
+var app = builder.Build(); 
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapOpenApi(); 
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapScalarApiReference();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
